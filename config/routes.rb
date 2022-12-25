c5d59873439f78f442774b119189fa4acce763da
Phantomvlad <mmvlad99@mail.ru>
2022-12-25 15:51:29 +0300

    Correction errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   root to: 'questions#index'
+
   resources :questions do
     put :hide, on: :member
   end
+
+  resource :session, only: %i[new create]
+  
+  resources :users, only: %i[new create]
 end
---
+++ db/migrate/20221221200752_add_column_phone_to_users.rb
@@ -0,0 +1,5 @@
+class AddColumnPhoneToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :phone, :integer
+  end
+end
