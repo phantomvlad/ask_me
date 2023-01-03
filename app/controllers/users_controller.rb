75b0f5080727188573dab886f66976dfda4ae50d
Phantomvlad <mmvlad99@mail.ru>
2023-01-03 15:20:05 +0300

    Fix buggs registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,12 @@ class UsersController < ApplicationController
   before_action :set_user, only: %i[show edit update destroy]
   before_action :authorise_user, only: %i[edit update destroy]
   def new
+    @user=User.new
   end
 
   def create
     @user = User.new(user_params)
+
     if @user.save
       session[:user_id] = @user.id
       redirect_to root_path, notice: "Вы успешно зарегистрировались!"
